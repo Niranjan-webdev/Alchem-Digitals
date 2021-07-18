e6ab558874fd3dac20fc5e11198a1ca7a6ae13cb
niranjang2 <niranjan.g2k@gmail.com>
2021-07-18 14:44:11 +0530

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,189 @@
 import logo from './logo.svg';
+
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <p className="head">Fellowship : sign up today</p>
+      <img className="logo" src="./assets/group 12.svg"/>
+
+      <div className="topnav">
+        <ul>
+          <li><a href="#">HOME</a></li>
+            <li><a href="#">SERVICES</a></li>
+              <li><a href="#">ONLINE COURSES</a></li>
+              <li><a href="#">MOCK TESTS</a></li>
+             <li><a href="#">PRODUCTS</a></li>
+           <li><a href="#">ABOUT US</a></li>
+          <li>
+            <button className="sign">Sign up</button>
+          </li>
+        </ul>
+      </div>
+ <section>
+ <div >
+      <img src="./assets/image 11.png"/>
+      <p className="para">Lorem ipsum dolor <br/> sit amet, consectetur</p>
+       <p className="para1">Lorem ipsum dolor sit amet, consectetur iluma <br/>asdientisu elite, de tor alikas tina moner </p>
+      
+      <button className="btn1">Learn more</button>
+     
+      </div>
+ </section>
+    
+      <section>
+      <div className="container">
+        <p className="para2">What CS offers</p>
+        <p className="para3">Lorem Ipsum is simply dummy text of the printing and typesetting<br/> industry. Lorem Ipsum has been the industry's standard dummy text<br/> ever 
+        since the 1500s, when an unknown printer.</p>
+        <p className="para4"> placeholder text for use in your graphic, print and web layouts, and<br/> discover plugins for your favorite writing, design and blogging <br/>tools. Explore the origins,
+         history and meaning. </p>
+
+         <p className="para5">typesetting industry. Lorem Ipsum has been the industry's standard<br/> dummy text when an unknown printer.</p>
+      
+        <div className="video"></div>
+
+        <h2 className="Benefits">Benefits</h2>
+
+         <div className="card">
+           <img className="ellipse1" src="./assets/ellipse 1.svg" height="40px"/>
+           <img className="ell1" src="./assets/i1.svg"/>
+           <h5>Single stop solution</h5>
+           <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever.</p>
+         </div> 
+
+          <div className="card1">
+          <img className="ellipse2" src="./assets/ellipse 9.svg" height="40px"/>
+          <img className="ell2" src="./assets/idea.svg"/>
+          <h5>Expertise</h5>
+          <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever.</p>
+          </div>
+
+          <div className="card2">
+          <img className="ellipse3" src="./assets/ellipse 9.svg" height="40px"/>
+          <img className="ell3" src="./assets/idea.svg"/>
+          <h5>We care</h5>
+          <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever.</p>
+          </div>
+
+      </div>
+      </section>
+ 
+ <section>
+     <div>
+       <div className="card3">
+          <img className="group20" src="./assets/group 20.png" height="150px"/>
+          <h3>Services</h3>
+          <p>Lorem Ipsum is simply dummy text of the printing <br/> and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley.</p>
+           <button>Learn more</button>
+       </div>
+
+       <div className="card4">
+        <img className="mock" src="./assets/mock.png" height="110px"/>
+        <h3>Mock Tests</h3>
+        <p>Lorem Ipsum is simply dummy text of the<br/> printing  ever since the 1500s, when an<br/> printer took a galley.</p>
+        <button>Learn more</button>
+       </div>
+
+       <div className="card5">
+        <img  src="./assets/image 8.png" height="120px"/>
+        <h3>Mock Tests</h3>
+        <p>Lorem Ipsum is simply dummy text of the<br/> printing  ever since the <br/>1500s, when an<br/> printer took a galley.</p>
+        <button>Learn more</button>
+       </div>
+     </div>
+     </section>
+
+   <section>
+     <div className="card6"> 
+       <h3>Products</h3>
+       <p>Lorem Ipsum is simply dummy text of the printing <br/> and typesetting industry. Lorem Ipsum has been the <br/>industry's standard dummy text ever since the<br/> 1500s, when an unknown printer took a galley.</p>
+       <button>Learn more</button>
+       <img src="./assets/image 10.png" height="150px"/>
+     </div>
+
+     </section>
+  
+  <section>
+    <div className="container1">
+      
+      <img className="image9" src="./assets/image 9.png" height="350px"/>
+      <div className="card7">
+         <img src="./assets/dffgrdt.png" height="170px"/>
+         <h3>Karthik Moorthi</h3>
+         <label>USC</label>
+         <p> graphic designers, occupying the spaces of a<br/> website, an advertising product or an editorial<br/> production whose final text is not yet ready.</p>  
+      </div>
+
+    </div>
+    </section>
+
+    <section>
+
+    <div className="container2">
+      <h2>CU Alumni</h2>
+      <div className="card8">
+       <img className="group15" src="./assets/group 15.svg"/>
+       <p>placeholder text for use in your graphic, print and web layouts, and discover plugins for your favorite writing,</p>
+        <img className="ellipse8" src="./assets/ellipse 8.svg" height="30px"/>
+        <h4>Dr. Divakar Anbalagan</h4>
+      </div>
+
+      <div className="card9">
+      <img className="group16" src="./assets/group 16.svg"/>
+      <p>placeholder text for use in your graphic, print and web layouts, and discover plugins for your favorite writing,</p>
+      <img className="ellipse8" src="./assets/ellipse 8.svg" height="30px"/>
+        <h4>Dr. Divakar Anbalagan</h4>
+      </div>
+
+      <div className="card10">
+      <img className="group15" src="./assets/group 15.svg"/>
+       <p>placeholder text for use in your graphic, print and web layouts, and discover plugins for your favorite writing,</p>
+        <img className="ellipse8" src="./assets/ellipse 8.svg" height="30px"/>
+        <h4>Dr. Divakar Anbalagan</h4>
+      </div>
+    </div>
+    </section>
+    
+    <div className="container3">
+    <img className="group" src="./assets/group 24.png" />
+    <p>Doing our best and that is our job for our clients </p>
+    <h2>NEWS LETTER</h2>
+    <input type="text"/>
+    <button>Subscribe</button>
+    </div>
+
+   <div className="container4" >
+    <img src="./assets/group 12.svg"/>
+    <p>filling text is used by graphic designers,<br/> programmers and printers with the<br/> aim of occupying the spaces of a<br/> website, an advertising product final.</p>
+  
+    <h4>Explore</h4>
+    <ul>
+      <li>Home</li>
+      <li>About us</li>
+      <li>Team</li>
+      <li>Inner circle</li>
+    </ul>
+     <h5>Links</h5> 
+
+     <div className="list">
+     <ul >
+       <li>Terms snd conditions</li>
+       <li>privacy pollicy</li>
+     </ul>
+     </div>
+     <h6>Connect</h6>
+    
+   </div>
+   <div className="icons">
+      <ul>
+        <li><img src="./assets/fb.svg"/></li>
+        <li><img src="./assets/yt.svg"/></li>
+        <li><img src="./assets/ig.svg"/></li>
+      </ul>
+    </div>
+
     </div>
   );
 }
